cbee685914351eac8a2023acac015ef418942345
Joel Thoms <jthoms@gmail.com>
2018-10-11 01:59:33 -0700

    create DEV.to CLI

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,19 @@
+import pipe from 'mojiscript/core/pipe'
 import log from 'mojiscript/console/log'
 import run from 'mojiscript/core/run'
+//import axios from 'mojiscript/net/axios'
 import main from './main'
+import { readUtf8File } from './interop/fs'
+
+const mockAxios = {
+  get: () => pipe ([
+    () => readUtf8File ('devto.html'),
+    data => ({ data })
+  ])
+}
 
 const dependencies = {
+  axios: mockAxios,
   log
 }
 
--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,21 @@
 import pipe from 'mojiscript/core/pipe'
+import map from 'mojiscript/list/map'
+import $ from 'mojiscript/string/template'
+import { getDevToHtml } from './api'
+import { getElements } from './interop/cheerio'
+import { parseElement } from './interop/parser'
 
-const main = ({ log }) => pipe ([
-  'Hello World',
-  log
+const formatPost = $`${'title'}
+${'url'}\n#${'tags'}
+${'username'} ・ 💖  ${'comments'} 💬  ${'reactions'}
+`
+
+const main = ({ axios, log }) => pipe ([
+  getDevToHtml (axios),
+  getElements ('.single-article:not(.feed-cta)'),
+  map (parseElement),
+  map (formatPost),
+  map (log)
 ])
 
 export default main
